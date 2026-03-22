223d83c13e4037eb025edcdca69bc87b48486a77
haiqalhasly <haiqalhasly@gmail.com>
2026-03-23 07:54:39 +0800

    updated task 2 lab 8

--- Lab8/Task2.R
+++ Lab8/Task2.R
@@ -1,22 +1,52 @@
-# Load required libraries
 library(ggplot2)
 library(dplyr)
+library(tidyr)
 
-# 1. Prepare the data: Calculate the average CO2 uptake for each region (Type)
-pie_data <- CO2 %>%
-  group_by(Type) %>%
-  summarise(avg_uptake = mean(uptake))
+data("EuStockMarkets")
 
-# 2. Create the Pie Chart
-ggplot(pie_data, aes(x = "", y = avg_uptake, fill = Type)) +
-  geom_bar(stat = "identity", width = 1, color = "white") +
-  coord_polar("y", start = 0) + # This converts the bar chart into a pie chart
+# Convert to data frame and add the time column
+eu_data <- as.data.frame(EuStockMarkets) %>%
+  mutate(time = as.numeric(time(EuStockMarkets)))
+
+# Reshape from "wide" to "long" format
+eu_long <- eu_data %>%
+  pivot_longer(
+    cols = c(DAX, SMI, CAC, FTSE), 
+    names_to = "Stock_Index", 
+    values_to = "Price"
+  )
+
+closing <- ggplot(eu_long, aes(x = time, y = Price, color = Stock_Index)) +
+  geom_line(linewidth = 0.8) +
   labs(
-    title = "Average CO2 Uptake by Region",
-    subtitle = "Quebec plants dominate in overall performance",
-    fill = "Plant Origin"
+    title = "Daily Closing Prices of Major European Stock Indices",
+    x = "Year",
+    y = "Closing Price",
+    color = "Market Index"
   ) +
-  # Add the actual numbers to the slices
-  geom_text(aes(label = round(avg_uptake, 1)), position = position_stack(vjust = 0.5), color = "white", fontface = "bold") +
-  theme_void() + # Removes the background, grid, and axes for a clean pie look
-  scale_fill_manual(values = c("Quebec" = "#2E86C1", "Mississippi" = "#E74C3C")) # Custom professional colors
+  theme_minimal()
+
+print(closing)
+
+
+distr <- ggplot(eu_long, aes(x = Price, fill = Stock_Index)) +
+  geom_density(alpha = 0.5) +
+  theme_minimal() +
+  labs(title = "Distribution of Stock Prices (1991-1998)",
+       x = "Closing Price",
+       y = "Density")
+
+
+print(distr)
+
+# Prepare data with a discrete Year column
+eu_yearly <- eu_data %>%
+  mutate(Year = as.factor(floor(time)))
+
+gerPrice <- ggplot(eu_yearly, aes(x = Year, y = DAX, fill = Year)) +
+  geom_boxplot() +
+  guides(fill = "none") +
+  theme_bw() +
+  labs(title = "German DAX Price Spread by Year")
+
+print(gerPrice)
