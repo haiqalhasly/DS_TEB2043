7ccf8874d11766afdbcb605e3d4776562fb036f2
haiqalhasly <haiqalhasly@gmail.com>
2026-03-23 07:29:54 +0800

    updated task 1 lab 8

--- Lab8/Task1.R
+++ Lab8/Task1.R
@@ -19,6 +19,7 @@ print(paste("num of row: ", nrow(titanic)))
 titanic_count <- 0
 
 survived = sum(titanic$Survived == 1)
+not_survived = sum(titanic$Survived == 0)
 print(paste("Number of people survived:", survived))
 
 first_class_surv <- 0
@@ -78,5 +79,34 @@ print(paste("Percentage 1st Class Female Survivors:", round(female_pct,2), "%"))
 print(paste("Percentage 1st Class Male Survivors:", round(male_pct,2), "%"))
 
 
-boxplot( titanic ~ survived, data = titanic, xlab = "Class", 
-       ylab = "Total Number", main = "Percentage of people survived by class") 
+# boxplot( titanic ~ survived, data = titanic, xlab = "Class", 
+#        ylab = "Total Number", main = "Percentage of people survived by class") 
+
+# Create a table of survival counts by class
+survival_by_class <- table(titanic$Survived, titanic$Pclass)
+
+# Plot a bar chart
+barplot(survival_by_class, 
+        beside = TRUE, 
+        legend.text = c("Did Not Survive", "Survived"),
+        args.legend = list(x = "topleft"),
+        xlab = "Passenger Class", 
+        ylab = "Total Number", 
+        main = "Number of People Survived by Class",
+        col = c("indianred", "lightgreen"))
+
+surv_by_sex<- table(titanic$Survived, titanic$Sex)
+
+barplot(surv_by_sex,
+        main = "Number of People Survived by Sex",
+        legend.text = c("Male", "Female"),
+        xlab = "",
+        col = rainbow(length(surv_by_sex))
+)
+
+pieSurvData <- c(survived, not_survived)
+piepercent <- round(100*pieSurvData/sum(pieSurvData), 1)
+
+pie(pieSurvData,labels = piepercent,main = "Number of people survived", col = rainbow(length(pieSurvData)))
+legend("topright",  c("Survived", "Not Survived"), cex = 0.8, fill = rainbow(length(pieSurvData)))
+        
